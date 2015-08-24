ea6f9a65056ce8188dfa7de80c0f1306e08691b9
themyth92 <themyth92@gmail.com>
2015-08-24 18:03:28 +0800

    Build directive instead of service and provider

--- src/css-rotating-plane/rot-plane.directive.js
+++ src/css-rotating-plane/rot-plane.directive.js
@@ -1,47 +1,20 @@
-(function(){
-
+// TODO : change style dynamically
+(function(angular) {
   'use strict';
-
-  angular
-    .module('ngSpin', [])
-    .directive('spRotatingPlane', RotatingPlane);
-
-    function RotatingPlane() {
-      
+  angular.module('ngSpin.rotPlane', [])
+    .directive('rotPlane', function directive() {
       return {
         restrict : 'AE',
+        replace : true,
         scope : {
-          options : '=spOptions',
-          show    : '=spRotatingPlane' 
-        },
+          spinWidth : '@',
+          spinHeight : '@',
+          spinSpeed : '@'
+        },  
+        template : '<div class="sk-spinner sk-spinner-rotating-plane"></div>',
+        link : function link(scope, elem, attrs) {
 
-        template : '<span class = "sk-spinner-rotating-plane sk-spinner" ng-show = "show"></span>',
-        link : function(scope, elem){
-
-          activate();
-
-          //============================
-          
-          function activate() {
-
-            changeCssStyleOnUserOption();
-          }
-
-          function changeCssStyleOnUserOption() {
-
-            if(scope.options.size) {
-
-              elem.css('width' : scope.options.size);
-              elem.css('height' : scope.options.size);
-            }
-
-            if(scope.options.speed) {
-
-              elem.css('-webkit-animation', 'sk-rotatePlane ' + scope.options.speed + 's infinite ease-in-out';
-              elem.css('animation', 'sk-rotatePlane' + scope.options.speed + 's infinite ease-in-out');
-            }
-          }
         }
       }
-    } 
-});
+    });
+})(angular);
